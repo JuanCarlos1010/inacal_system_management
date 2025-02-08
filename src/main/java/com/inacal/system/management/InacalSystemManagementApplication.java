e78e367361158624fbc99038f0933d9a7e33d846
JuanCarlos1010 <jheann.elec@gmail.com>
2025-02-08 12:45:34 -0500

    feat: init project

---
+++ src/main/java/com/inacal/system/management/InacalSystemManagementApplication.java
@@ -0,0 +1,13 @@
+package com.inacal.system.management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InacalSystemManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InacalSystemManagementApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/inacal/system/management/InacalSystemManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inacal.system.management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InacalSystemManagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
